2586e0440d5d3642dea16247a081fc9dc15419d4
snilssonboy <simon.nilsson@dohi.se>
2016-01-25 23:07:20 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var time = 0;
+
+function stepup(){
+	
+}
+
+$(function() {
+        $(".dial").knob();
+});
+
+setInterval(function(){ 
+	if(time < 100){
+		time += 0.1;
+	}else{
+		time = 0;
+	}
+
+ 	$(".dial")
+ 		.val(time)
+        .trigger('change');
+
+ }, 10);
